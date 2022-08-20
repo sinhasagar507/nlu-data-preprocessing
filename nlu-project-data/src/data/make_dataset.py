7e8dd10327bec450c0e61ae582fa9f0ae46712b1
Sagar Sinha <sinhasagar507@gmail.com>
2022-08-20 23:22:14 +0530

    Added preprocessing function

--- nlu-project-data/src/data/make_dataset.py
+++ nlu-project-data/src/data/make_dataset.py
@@ -1,8 +1,79 @@
 # -*- coding: utf-8 -*-
-import click
+
+# Primary Libraries
+import re
 import logging
 from pathlib import Path
+
+
+import click
 from dotenv import find_dotenv, load_dotenv
+import contractions as cm
+
+
+def clean(
+        text, newline=True, quote=True,
+        bullet_point=True, dates=True, link=True,
+        strikethrough=True, spoiler=True, heading=True,
+        emoj=True, emoticon=True, condensed=True):
+    # Newlines we don't need - only
+    """ Cleans reddit utterances"""
+
+    if newline:
+        text = re.sub(r'\n+', ' ', text)
+        # Remove the many " " that we replaced in the last step
+        text = text.strip()
+        text = re.sub(r'\s\s+', ' ', text)
+
+    # > are for the quoted texts from the main comment or the reply
+    if quote:
+        text = re.sub(r'>', '', text)
+
+    # Bullet points/asterisk are used for markdown like - bold/italic - Could create trouble in parsing? idk
+    if bullet_point:
+        text = re.sub(r'\*', '', text)
+        text = re.sub('&amp;#x200B;', '', text)
+
+    # []() Link format then we remove both the tag/placeholder and the link
+    if link:
+        text = re.sub(r"http\S+", '', text)
+        text = re.sub(r'\[.*?\]\(.*?\)', '', text)
+
+    # Strikethrough
+    if strikethrough:
+        text = re.sub('~', '', text)
+
+    # Spoiler, which is used with < less-than (Preserves the text)
+    if spoiler:
+        text = re.sub('&lt;', '', text)
+        text = re.sub(r'!(.*?)!', r'\1', text)
+
+    # Heading to be removed as there are these markdown style features in reddit too
+    if heading:
+        text = re.sub('#', '', text)
+
+    if emoj:
+        # Implement the emoji scheme here
+        # Makes more sense for the node feature but might as well import that function here if ready
+        # Implementing a Naive Emoji Scheme
+        # Some associated libraries are EMOT and DEMOJI
+        # text = emoji.demojize(text).replace(":", "").replace("_", "")
+        pass
+
+    if dates:
+        text = re.sub(r'(\d+/\d+/\d+)', '', text)
+
+    if emoticon:
+        # Implement the emoticon scheme here.
+        # Makes more sense for the node feature but might as well import that function here if ready
+        pass
+
+    # Needs to be the last step in the process
+    if condensed:
+        text = cm.fix(text)
+        # print("Running")
+
+    return text
 
 
 @click.command()
