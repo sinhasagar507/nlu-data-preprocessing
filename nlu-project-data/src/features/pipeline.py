89905a5ba8c8636d90f18b80ae60a7cc6a7f14ea
Sagar Sinha <sinhasagar507@gmail.com>
2022-09-04 12:41:20 +0530

    corpus dump

--- nlu-project-data/src/features/pipeline.py
+++ nlu-project-data/src/features/pipeline.py
@@ -1,45 +1,56 @@
-from convokit import Corpus
-from n
-from convokit.text_processing import TextProcessor, TextCleaner
-from convokit.convokitPipeline import ConvokitPipeline
-from src.data.make_dataset import clean
-from build_heuristic_features import convert_to_lower, sentiment_analyzer, normalized_modifier_cnt, \
-    normalized_hedge_cnt
-from build_heuristic_features import subjectivity_score, normalized_group_ref_cnt, subjectivity_score, \
-    measure_politeness
+try:
+    import os
+
+    from convokit import Corpus
+
+    from convokit.text_processing import TextProcessor, TextCleaner
+    from convokit.convokitPipeline import ConvokitPipeline
+    from src.data.make_dataset import clean
+    from src.__init__ import ROOT_DIR
+    from heuristic_features import convert_to_lower, measure_sentiment, normalized_modifier_cnt, \
+        normalized_hedge_cnt, measure_subjectivity, normalized_group_ref_cnt, measure_politeness
+
+except Exception as e:
+    print(e)
 
 
-# The following pipe would be expanded further
-clean_reddit_text = TextCleaner(text_cleaner=clean, replace_text=False, save_original=True)
-lowercased_text = TextProcessor(proc_fn=convert_to_lower, output_field="lowercase_text")
-analyze_sentiment = TextProcessor(proc_fn=sentiment_analyzer, input_field="lowercase_text",
-                                  output_field="sentiment_polarity")
-score_subjectivity = TextProcessor(proc_fn=subjectivity_score, input_field="lowercase_text", output_field="subjectivity_score")
-modifier_count = TextProcessor(proc_fn=normalized_modifier_cnt, input_field="lowercase_text",
-                               output_field="normalized_modifier_count")
-hedge_modals = TextProcessor(proc_fn=normalized_hedge_cnt, input_field="lowercase_text",
-                             output_field="normalized_hedge_cnt")
-group_ref_count = TextProcessor(proc_fn=normalized_group_ref_cnt, input_field="lowercase_text",
-                                output_field="normalized_group_ref_cnt")
-politeness_markers = TextProcessor(proc_fn=measure_politeness, input_field="lowercase_text",
-                                   output_field="politeness_markers")
+def main():
+    clean_text = TextCleaner(text_cleaner=clean, replace_text=False, save_original=True)
+    lowercased_text = TextProcessor(proc_fn=convert_to_lower, output_field="lowercase_text")
+    analyze_sentiment = TextProcessor(proc_fn=measure_sentiment, input_field="lowercase_text",
+                                      output_field="sentiment_polarity")
+    analyze_subjectivity = TextProcessor(proc_fn=measure_subjectivity, input_field="lowercase_text",
+                                         output_field="subjectivity_score")
+    modifiers = TextProcessor(proc_fn=normalized_modifier_cnt, input_field="lowercase_text",
+                              output_field="normalized_modifier_count")
+    hedges = TextProcessor(proc_fn=normalized_hedge_cnt, input_field="lowercase_text",
+                           output_field="normalized_hedge_cnt")
+    pronouns = TextProcessor(proc_fn=normalized_group_ref_cnt, input_field="lowercase_text",
+                             output_field="normalized_group_ref_cnt")
+    politeness = TextProcessor(proc_fn=measure_politeness, input_field="lowercase_text",
+                               output_field="politeness_markers")
 
-pipe_reddit_uncleaned = ConvokitPipeline([
-    ("convert to lowercase", lowercased_text),
-    ("sentiment analyzer", analyze_sentiment),
-    ("count modifiers", modifier_count),
-    ("count hedges and modals", hedge_modals),
-    ("count group references", group_ref_count),
-    ("subjectivity score", subjectivity_score),
-    ("politeness indicators", politeness_markers)
-])
+    discriminative_feature_pipe = ConvokitPipeline([  # The following pipe would be expanded further
+        ("lowercase", lowercased_text),
+        ("sentiment", analyze_sentiment),
+        ("subjectivity", analyze_subjectivity)
+        ("modifiers", modifiers),
+        ("hedges", hedges)
+        ("pronouns", pronouns),
+        ("politeness", politeness),
+    ])
 
-import pandas as pd
-data = pd.read_csv("nlu-project-data/data/raw/submissions/sample_submission.csv")
-# Load the corpus
-corpus = Corpus(filename=)
+    # Load the corpus
+    sample_corpus = "sample_corpus"
+    BASE_PATH = os.path.join(ROOT_DIR, "data", "interim", "unclean")
+    corpus = Corpus(sample_corpus, filename=BASE_PATH)
+    uncleaned_sample_corpus = discriminative_feature_pipe.transform(corpus)
+    cleaned_sample_corpus = clean_text.transform(uncleaned_sample_corpus)
 
-# Finally, clean the reddit data
-cleaned_corpus =
+    # Dump Corpus here
+    dump_dir = os.path.join(ROOT_DIR, "data", "interim", "interim", "clean")
+    corpus.dump(cleaned_sample_corpus, dump_dir)
 
 
+if __name__ == "__main__":
+    main()
