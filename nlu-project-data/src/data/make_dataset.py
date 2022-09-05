b9f2f19e9da1f3335bdb86ef7f177f9e234ad387
Sagar Sinha <sinhasagar507@gmail.com>
2022-09-05 17:54:36 +0530

    bot removal function added

--- nlu-project-data/src/data/make_dataset.py
+++ nlu-project-data/src/data/make_dataset.py
@@ -4,14 +4,18 @@
 import re
 import logging
 
-import contractions as cm
 import click
+import contractions as cm
 
 from pathlib import Path
+from typing import List, Dict
+from tqdm import tqdm
+from pandas import DataFrame
+from convokit import Corpus
 from dotenv import find_dotenv, load_dotenv
 
 
-def clean(
+def cleantext(
         text, newline=True, quote=True,
         bullet_point=True, dates=True, link=True,
         strikethrough=True, spoiler=True, heading=True,
@@ -74,6 +78,98 @@ def clean(
         # print("Running")
 
     return text
+
+
+def createConversationGraph(graph: {}, child_list: List[str], parent_list: List[str]) -> Dict[str: List[str]]:
+    """
+    Creates a adjacency list of parent comment ids and child comment ids
+    Args:
+        graph: empty dictionary
+        child_list: children comment ids
+        parent_list: parent comment ids
+
+    Returns:
+        graph: conversation adjacency list
+
+    """
+    for child, parent in zip(child_list, parent_list):
+
+        if parent not in graph.keys():
+            graph[parent] = []
+            graph[parent].append(child)
+        else:
+            graph[parent].append(child)
+
+    return graph
+
+
+def identify_bots(corpus: Corpus) -> List[str]:
+    """
+    Use a regex expression to search for explicit bots.
+
+    Args :
+      Conversation object
+
+    Returns :
+      List : a list of bots, if any from the conversation
+
+    """
+
+    remove = ["AutoModerator", "[deleted]"]
+    blacklist = []
+    speakers = corpus.get_speaker_ids()
+    for speaker in speakers:
+        result = re.search("\w+[^Ro|ro|][bB][oO][tT]$", speaker)
+        if result:
+            blacklist.append(speaker)
+
+    blacklisted_speakers = remove + blacklist
+    return blacklisted_speakers
+
+
+def recurse_utterance_ids(graph: Dict[None: None], remove_utt_ids: List, iD: str) -> List:
+    """
+    Stores utterance IDs for all blacklisted speaker IDs  and their child speaker IDs
+
+    Args:
+        graph: conversation graph adjacency list
+        remove_utt_ids: utterance ids with blacklisted speakers
+        iD: utterance iD
+
+
+    Returns:
+        List of visited Ids that need to be removed from the utterances dataframe
+
+    """
+    remove_utt_ids.append(str(iD))
+
+    if iD in list(graph.keys()):
+        for idx in tqdm(graph[str(iD)]):
+            recurse_utterance_ids(graph, remove_utt_ids, idx)
+    else:
+        return remove_utt_ids
+
+
+def bot_removal(graph: Dict[str: List[str]], utt_df: DataFrame, blacklisted_speakers: List[str]) -> List[str]:
+    """
+    Role:
+        Retrieve comment Ids of blacklisted speakers and its associated replies
+
+    Args:
+        graph: conversation graph adjacency list
+        utt_df: utterances dataframe
+        blacklisted_speakers: list of auto moderators, deleted utterance speakers and explicit bots
+
+    Returns:
+        List of utterance ids of blacklisted speakers and associated replies
+
+    """
+    remove_utt_ids = []
+    for idx, row in utt_df.iterrows():
+        if row["speaker"] in blacklisted_speakers:
+            utt_ids = recurse_utterance_ids(graph, remove_utt_ids, idx)
+
+    return utt_ids
 
 
 @click.command()
