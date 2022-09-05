c9c6ea27a7b8fd90ea5a25c927c74225f1cd5265
Sagar Sinha <sinhasagar507@gmail.com>
2022-09-05 17:59:18 +0530

    global constants

--- nlu-project-data/src/__init__.py
+++ nlu-project-data/src/__init__.py
@@ -1,3 +1,7 @@
 import os
-
 ROOT_DIR = os.path.realpath(os.path.dirname(__file__))
+developer_key_1 = os.environ.get("GOOGLE_API_CLIENT_INSTITUTION")
+developer_key_2 = os.environ.get("GOOGLE_API_CLIENT_PRIMARY")
+developer_key_3 = os.environ.get("GOOGLE_API_CLIENT_506")
+developer_key_4 = os.environ.get("GOOGLE_API_CLIENT_508")
+developer_key_5 = os.environ.get("GOOGLE_API_CLIENT_509")
